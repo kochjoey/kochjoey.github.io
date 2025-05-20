51cbaf0c837b5878fdd9aea8c58c2af1bc65249d
kochjoey <kochjoey@gmail.com>
2025-05-19 19:51:41 -0700

    Fix underline on portfolio and make portfolio images interactive

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const image2 = document.getElementById('about-img2');
 const image3 = document.getElementById('about-img3');
 const image4 = document.getElementById('about-img4');
 
+// CHECK TO SEE IF WE CAN CLEAN THIS UP USING OTHER SELECTORS TO GE THE CHILD OF THE IMG DIV CAUSE THEY ALL HAVE THE SAME CLASS AND STRUCTURE BUT RIGHT NOW THE JS IS BASED ON THEIR UNIQUE ID NAMES
+
 image1.addEventListener('mouseover', function() {
     document.getElementById('desc-1').style.display = 'block';
 });
